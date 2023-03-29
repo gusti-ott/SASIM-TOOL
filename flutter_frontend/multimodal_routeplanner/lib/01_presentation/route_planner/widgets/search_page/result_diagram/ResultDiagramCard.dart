bdaf6c43ab5069d7620384f517731aad7d2b05df
gusti-ott <g.ottrubay@gmail.com>
2023-03-29 13:44:48 +0200

    don't show external costs information, if chart bar is clicked on

--- flutter_frontend/multimodal_routeplanner/lib/01_presentation/route_planner/widgets/search_page/result_diagram/ResultDiagramCard.dart
+++ flutter_frontend/multimodal_routeplanner/lib/01_presentation/route_planner/widgets/search_page/result_diagram/ResultDiagramCard.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:multimodal_routeplanner/01_presentation/route_planner/widgets/search_page/result_diagram/CostDetails.dart';
 import 'package:multimodal_routeplanner/03_domain/enums/DiagramTypeEnum.dart';
 
-import '../../../../../02_application/bloc/cost_details_bloc.dart';
 import '../../../../../02_application/bloc/diagram_type_bloc.dart';
 import '../../../../../03_domain/entities/Trip.dart';
-import 'CostDetailsText.dart';
 import 'ResultDiagramBar.dart';
 import 'TitleDropdown.dart';
 
@@ -33,18 +29,15 @@ class ResultDiagramCard extends StatelessWidget {
                 child: BlocBuilder<DiagramTypeBloc, DiagramTypeState>(
                   builder: (context, state) {
                     if (state is DiagramTypeSelected) {
-                      return ResultDiagramBarWidget(
-                          trips: trips, animate: true, diagramType: state.type);
+                      return ResultDiagramBarWidget(trips: trips, animate: true, diagramType: state.type);
                     } else {
                       return ResultDiagramBarWidget(
-                          trips: trips,
-                          animate: true,
-                          diagramType: DiagramTypeEnum.externalCosts);
+                          trips: trips, animate: true, diagramType: DiagramTypeEnum.externalCosts);
                     }
                   },
                 ),
               ),
-              BlocBuilder<CostDetailsBloc, CostDetailsState>(
+              /*BlocBuilder<CostDetailsBloc, CostDetailsState>(
                 builder: (context, state) {
                   if (state is CostDetailsLoadedState) {
                     return CostDetails(costs: state.costs);
@@ -54,7 +47,7 @@ class ResultDiagramCard extends StatelessWidget {
                     return const CostDetailsText();
                   }
                 },
-              ),
+              ),*/
             ],
           ),
         ),
