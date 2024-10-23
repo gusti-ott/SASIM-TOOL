15623fadd2604cc776b98e2410707ddb40b36877
gusti-ott <g.ottrubay@gmail.com>
2024-10-23 23:03:44 +0200

    add simple tracking and update docker compose file

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,8 @@
+import csv
 import os
+from datetime import datetime
+
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, request, redirect, jsonify
 from flask import render_template
 from flask import send_from_directory
@@ -27,6 +31,61 @@ cache = Cache(server)
 CORS(server)
 
 FLUTTER_WEB_APP = 'templates'
+
+# In-memory storage for detailed logs and daily summary
+detailed_logs = []
+daily_summary = {
+    "date": datetime.now().strftime('%Y-%m-%d'),
+    "total_calls": 0,
+    "successful_calls": 0,
+    "error_calls": 0
+}
+
+# Scheduler for daily log writing task
+scheduler = BackgroundScheduler()
+
+
+@scheduler.scheduled_job('cron', hour=0)
+def save_logs_to_csv():
+    global detailed_logs, daily_summary
+
+    # Ensure 'tracking' directory exists
+    if not os.path.exists('tracking'):
+        os.makedirs('tracking')
+
+    # Write detailed logs to CSV file
+    detailed_log_file = 'tracking/detailed_logs.csv'
+    file_exists = os.path.isfile(detailed_log_file)
+
+    with open(detailed_log_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(['Date', 'Start Address', 'End Address', 'Mode'])  # Write header only once
+        for log in detailed_logs:
+            writer.writerow([log['date'], log['start_address'], log['end_address'], log['mode'], log['success']])
+
+    # Write daily summary to CSV file
+    summary_file = 'tracking/daily_summary.csv'
+    file_exists = os.path.isfile(summary_file)
+
+    with open(summary_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(['Date', 'Total Calls', 'Successful Calls', 'Error Calls'])  # Write header only once
+        writer.writerow([daily_summary['date'], daily_summary['total_calls'], daily_summary['successful_calls'], daily_summary['error_calls']])
+
+    # Reset in-memory logs for the next day
+    detailed_logs = []
+    daily_summary = {
+        "date": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        "total_calls": 0,
+        "successful_calls": 0,
+        "error_calls": 0
+    }
+    print('Logs saved to CSV at ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+
+scheduler.start()
 
 
 @server.route('/web/')
@@ -80,6 +139,11 @@ def return_otp_trip():
 
 @server.route('/plattform', methods=['GET'])
 def return_trip():
+    global daily_summary
+
+    # increment for each call of this endpoint
+    daily_summary['total_calls'] += 1
+
     if request.method == 'OPTIONS':
         # CORS preflight request handler
         response = jsonify({'status': 'CORS preflight successful'})
@@ -91,46 +155,89 @@ def return_trip():
     api_helper = ApiHelper()
 
     input_start_address = str(request.args['inputStartAddress'])
-    # start_location = get_geolocation(input_start_address)
-    start_location_and_id = get_efa_geolocation(input_start_address)
-    start_location = start_location_and_id[0]
-    start_id = start_location_and_id[1]
-
     input_end_address = str(request.args['inputEndAddress'])
-    # end_location = get_geolocation(input_end_address)
-    end_location_and_id = get_efa_geolocation(input_end_address)
-    end_location = end_location_and_id[0]
-    end_id = end_location_and_id[1]
-
     input_trip_mode = str(request.args['tripMode'])
-    trip_mode = api_helper.get_trip_mode_from_input(input_trip_mode)
-    quick_response_arg = request.args.get('quickResponse')
-    if quick_response_arg is None or quick_response_arg.strip() == '':
-        quick_response = False
-    else:
-        quick_response = quick_response_arg.lower() == 'true'
 
-    df_sharing_vehicles = get_efa_sharing_vehicles(start_location, quick_response)
+    try:
+        # start_location = get_geolocation(input_start_address)
+        start_location_and_id = get_efa_geolocation(input_start_address)
+        start_location = start_location_and_id[0]
+        start_id = start_location_and_id[1]
 
-    trip_controller = TripController(df_sharing_vehicles)
-    trip = trip_controller.get_trip(
-        start_location=start_location,
-        end_location=end_location,
-        trip_mode=trip_mode,
-        start_id=start_id,
-        end_id=end_id,
-        df_sharing_vehicels=df_sharing_vehicles
-    )
+        # end_location = get_geolocation(input_end_address)
+        end_location_and_id = get_efa_geolocation(input_end_address)
+        end_location = end_location_and_id[0]
+        end_id = end_location_and_id[1]
 
-    list_segments = []
+        trip_mode = api_helper.get_trip_mode_from_input(input_trip_mode)
+        quick_response_arg = request.args.get('quickResponse')
+        if quick_response_arg is None or quick_response_arg.strip() == '':
+            quick_response = False
+        else:
+            quick_response = quick_response_arg.lower() == 'true'
 
-    segments = trip.segments
+        df_sharing_vehicles = get_efa_sharing_vehicles(start_location, quick_response)
 
-    # if trip consists of multiple segments, iterate over segments to return a list of segments in the rest-api response
-    if type(segments) == list:
-        for j in range(len(trip.segments)):
+        trip_controller = TripController(df_sharing_vehicles)
+        trip = trip_controller.get_trip(
+            start_location=start_location,
+            end_location=end_location,
+            trip_mode=trip_mode,
+            start_id=start_id,
+            end_id=end_id,
+            df_sharing_vehicels=df_sharing_vehicles
+        )
+        list_segments = []
 
-            new_segment: Segment = trip.segments[j]
+        segments = trip.segments
+
+        # if trip consists of multiple segments, iterate over segments to return a list of segments in the rest-api response
+        if type(segments) == list:
+            for j in range(len(trip.segments)):
+
+                new_segment: Segment = trip.segments[j]
+                segment_waypoints = []
+                for k in range(len(new_segment.waypoints)):
+                    lat = new_segment.waypoints[k].lat
+                    lon = new_segment.waypoints[k].lon
+
+                    dict_waypoint = {
+                        'lat': float(lat),
+                        'lon': float(lon)
+                    }
+
+                    segment_waypoints.append(dict_waypoint)
+
+                dict_segment = {
+
+                    'mode': new_segment.mode.value,
+                    'distance': new_segment.distance,
+                    'duration': new_segment.duration,
+                    'costs': {
+                        'externalCosts': {
+                            'all': new_segment.costs.external_costs.external_costs,
+                            'air': new_segment.costs.external_costs.air,
+                            'noise': new_segment.costs.external_costs.noise,
+                            'climate': new_segment.costs.external_costs.climate,
+                            'accidents': new_segment.costs.external_costs.accidents,
+                            'space': new_segment.costs.external_costs.space,
+                            'barrier': trip.costs.external_costs.barrier,
+                            'congestion': trip.costs.external_costs.congestion
+                        },
+                        'internalCosts': {
+                            'all': new_segment.costs.internal_costs.variable + new_segment.costs.internal_costs.fixed,
+                            'variable': new_segment.costs.internal_costs.variable,
+                            'fixed': new_segment.costs.internal_costs.fixed}
+                    },
+                    'waypoints': segment_waypoints
+
+                }
+
+                list_segments.append(dict_segment)
+
+        else:
+            new_segment = segments
+
             segment_waypoints = []
             for k in range(len(new_segment.waypoints)):
                 lat = new_segment.waypoints[k].lat
@@ -160,7 +267,7 @@ def return_trip():
                         'congestion': trip.costs.external_costs.congestion
                     },
                     'internalCosts': {
-                        'all': new_segment.costs.internal_costs.variable + new_segment.costs.internal_costs.fixed,
+                        'all': new_segment.costs.internal_costs.variable + new_segment.costs.variable.variable,
                         'variable': new_segment.costs.internal_costs.variable,
                         'fixed': new_segment.costs.internal_costs.fixed}
                 },
@@ -170,77 +277,57 @@ def return_trip():
 
             list_segments.append(dict_segment)
 
-    else:
-        new_segment = segments
-
-        segment_waypoints = []
-        for k in range(len(new_segment.waypoints)):
-            lat = new_segment.waypoints[k].lat
-            lon = new_segment.waypoints[k].lon
-
-            dict_waypoint = {
-                'lat': float(lat),
-                'lon': float(lon)
-            }
-
-            segment_waypoints.append(dict_waypoint)
-
-        dict_segment = {
-
-            'mode': new_segment.mode.value,
-            'distance': new_segment.distance,
-            'duration': new_segment.duration,
+        dict_new_result = {
+            'tripMode': trip_mode.value,
+            'distance': trip.distance,
+            'duration': trip.duration,
             'costs': {
                 'externalCosts': {
-                    'all': new_segment.costs.external_costs.external_costs,
-                    'air': new_segment.costs.external_costs.air,
-                    'noise': new_segment.costs.external_costs.noise,
-                    'climate': new_segment.costs.external_costs.climate,
-                    'accidents': new_segment.costs.external_costs.accidents,
-                    'space': new_segment.costs.external_costs.space,
+                    'all': trip.costs.external_costs.external_costs,
+                    'air': trip.costs.external_costs.air,
+                    'noise': trip.costs.external_costs.noise,
+                    'climate': trip.costs.external_costs.climate,
+                    'accidents': trip.costs.external_costs.accidents,
+                    'space': trip.costs.external_costs.space,
                     'barrier': trip.costs.external_costs.barrier,
                     'congestion': trip.costs.external_costs.congestion
                 },
-                'internalCosts': {
-                    'all': new_segment.costs.internal_costs.variable + new_segment.costs.variable.variable,
-                    'variable': new_segment.costs.internal_costs.variable,
-                    'fixed': new_segment.costs.internal_costs.fixed}
+                'internalCosts': {'all': trip.costs.internal_costs.variable + trip.costs.internal_costs.fixed,
+                                  'variable': trip.costs.internal_costs.variable,
+                                  'fixed': trip.costs.internal_costs.fixed}
+
             },
-            'waypoints': segment_waypoints
+            'mobiScore': trip.mobi_score.value,
+            'segments': list_segments
 
         }
 
-        list_segments.append(dict_segment)
+        response = jsonify(dict_new_result)
+        response.headers.add('Access-Control-Allow-Origin', '*')
 
-    dict_new_result = {
-        'tripMode': trip_mode.value,
-        'distance': trip.distance,
-        'duration': trip.duration,
-        'costs': {
-            'externalCosts': {
-                'all': trip.costs.external_costs.external_costs,
-                'air': trip.costs.external_costs.air,
-                'noise': trip.costs.external_costs.noise,
-                'climate': trip.costs.external_costs.climate,
-                'accidents': trip.costs.external_costs.accidents,
-                'space': trip.costs.external_costs.space,
-                'barrier': trip.costs.external_costs.barrier,
-                'congestion': trip.costs.external_costs.congestion
-            },
-            'internalCosts': {'all': trip.costs.internal_costs.variable + trip.costs.internal_costs.fixed,
-                              'variable': trip.costs.internal_costs.variable,
-                              'fixed': trip.costs.internal_costs.fixed}
+        daily_summary['successful_calls'] += 1
 
-        },
-        'mobiScore': trip.mobi_score.value,
-        'segments': list_segments
+        log_entry = {
+            "date": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "start_address": input_start_address,
+            "end_address": input_end_address,
+            "mode": input_trip_mode,
+            "success": "true"
+        }
+        detailed_logs.append(log_entry)
 
-    }
+        return response
 
-    response = jsonify(dict_new_result)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    
-    return response
+    except Exception as e:
+        daily_summary['error_calls'] += 1
+        log_entry = {
+            "date": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            "start_address": input_start_address,
+            "end_address": input_end_address,
+            "mode": input_trip_mode,
+            "success": "false"
+        }
+        detailed_logs.append(log_entry)
 
 
 @cache.memoize(300)
